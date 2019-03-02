0cf42cddce338959bc0ded8e0e952fde5de21d66
Carley Tripp <carley.tripp@outlook.com>
2019-03-02 00:15:18 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name)
+  end
+
+  def artist_name
+    self.artist.name
+  end
 end
