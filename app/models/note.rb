e2c45e38b399442fe83342f1cb5dc72b63ef422d
Carley Tripp <carley.tripp@outlook.com>
2019-03-02 00:30:29 +0000

    Automatically backed up by Learn

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,10 +1,3 @@
 class Note < ActiveRecord::Base
   belongs_to :song
-
-  def song_notes=(notes)
-    notes.each do |note|
-      note = Note.find(note)
-      self.songs << note
-    end
-  end
 end
